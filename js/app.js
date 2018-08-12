5dd61057a9bc5b805803093f00fbbda92214b8c5
Jeffrey Barnes <belgerion@gmail.com>
2018-08-11 18:06:44 -0600

    testing android usage

--- js/app.js
+++ js/app.js
@@ -14,6 +14,12 @@
 			'date': '#',
 			'content': '#'
 		},*/
+          {
+            'title': 'Learning Overload',
+            'author': 'Jeffrey Barnes',
+            'date': 'August 11, 2018',
+            'content': 'This is a test to see if I can write and upload a blog post to Github from my Android phone.'
+          },
 		{
 			'title': 'New Project',
 			'author': 'Jeffrey Barnes',
